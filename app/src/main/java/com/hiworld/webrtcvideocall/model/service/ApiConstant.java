c9fdfdea310c3aa60485950196a36a2596f1837b
DESKTOP-M9OG4FM\Trong Dai <daint@hiworld.com.vn>
2017-06-02 10:55:37 +0700

    Remove retrofit

--- app/src/main/java/com/hiworld/webrtcvideocall/model/service/ApiConstant.java
+++
@@ -1,9 +0,0 @@
-package com.hiworld.webrtcvideocall.model.service;
-
-/**
- * Created by daint on 12/17/2016.
- */
-
-public class ApiConstant {
-    public static final String BASE_URL = "http://35.161.100.32/";
-}
